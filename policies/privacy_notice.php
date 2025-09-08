68acf2cb32573f0a198f4f2f34e92dba8b72ebbf
Jacqueline-Lim <jacquelinelim0513@gmail.com>
2025-09-08 18:07:39 +0800

    Change title

--- policies/privacy_notice.php
+++ policies/privacy_notice.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>About Us</title>
+  <title>Privacy Notice</title>
   <link rel="stylesheet" href="../css/styles.css">
   <link rel="stylesheet" href="../css/privacy_notice_styles.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
--- policies/return_refund.php
+++ policies/return_refund.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>About Us</title>
+  <title>Return & Refund Policy</title>
   <link rel="stylesheet" href="../css/styles.css">
   <link rel="stylesheet" href="../css/return_refund_styles.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
