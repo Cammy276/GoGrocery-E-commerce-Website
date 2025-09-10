f40f6710bf87bd09f8dfc8a2a0d797654f7dc56e
Jacqueline-Lim <jacquelinelim0513@gmail.com>
2025-09-10 14:14:21 +0800

    fix fogetpassword and make about us page responsive

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'db7c9845b4f45cc50a908965f29302ed3ec2c3c8',
+        'reference' => '8d327a94ba78d993709f64dd8aa40bdfb60c5a69',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'db7c9845b4f45cc50a908965f29302ed3ec2c3c8',
+            'reference' => '8d327a94ba78d993709f64dd8aa40bdfb60c5a69',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
