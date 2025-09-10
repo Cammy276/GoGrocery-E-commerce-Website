f05dbe112450a0ed9cc98e52088a253dc0fdac57
Jacqueline-Lim <jacquelinelim0513@gmail.com>
2025-09-10 23:33:41 +0800

    change word

--- policies/shipping_policies.php
+++ policies/shipping_policies.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Shipping Policies</title>
+  <title>Shipping Policy</title>
   <header>
     <?php include '../header.php'; ?>
   </header>
