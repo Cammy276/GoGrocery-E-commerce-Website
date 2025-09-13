7f61ecbee323bcc044a6eac984480f96f563fe09
Jacqueline-Lim <jacquelinelim0513@gmail.com>
2025-09-13 14:54:24 +0800

    Change login styles

--- auth/register.php
+++ auth/register.php
@@ -67,6 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </header>
     <link rel="stylesheet" href="../css/header_styles.css">
     <link rel="stylesheet" href="../css/register_styles.css">
+    <link rel="stylesheet" href="../css/footer_styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
