5d583b7a7a21a1e3a895649737c4e4771a73f894
Jacqueline-Lim <jacquelinelim0513@gmail.com>
2025-09-14 10:49:53 +0800

    delete toggle password in forgot_password

--- auth/forgot_password.php
+++ auth/forgot_password.php
@@ -65,21 +65,7 @@
             if (!isValid) {
                 event.preventDefault();
             }
-        });
-        function togglePassword(inputId, iconId) {
-            const input = document.getElementById(inputId);
-            const icon = document.getElementById(iconId);
-            if (input.type === "password") {
-                input.type = "text";
-                icon.classList.remove("bi-eye-fill");
-                icon.classList.add("bi-eye-slash-fill");
-            } else {
-                input.type = "password";
-                icon.classList.remove("bi-eye-slash-fill");
-                icon.classList.add("bi-eye-fill");
-            }
-        }
-        
+        });  
     </script>
     <?php include '../footer.php'; ?>
 </body>
